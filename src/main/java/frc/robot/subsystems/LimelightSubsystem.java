c3d21e1865be7edbd90c38fa10a4e8b06b95c265
valid-crashout <nate.k.huynh@gmail.com>
2025-05-07 16:27:26 -0700

    Speed Adjustments

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -7,7 +7,7 @@ public class LimelightSubsystem
 {
     public static double limelight_aim_proportional()
     {    
-        double kP = -.01; //Lower? Invert? 
+        double kP = -.005; //Lower? Invert? 
         double targetingAngularVelocity = LimelightHelpers.getTX("limelight-tigears") * kP;
         targetingAngularVelocity *= SwerveConstants.k_maxAngularSpeedRadiansPS;
         targetingAngularVelocity *= -1.0; //Invert?
@@ -16,8 +16,8 @@ public class LimelightSubsystem
 
     public static double limelight_range_proportional()
     {    
-        double kP = .01; //lower?
-        double targetingForwardSpeed = LimelightHelpers.getTY("limelight-tigears") * kP; //Look into TA
+        double kP = .0009; //lower?
+        double targetingForwardSpeed = (LimelightHelpers.getTA("limelight-tigears") != 0) ? Math.max(40 - LimelightHelpers.getTA("limelight-tigears"), 0) * kP : 0; //Look into TA
         targetingForwardSpeed *= SwerveConstants.k_maxWheelSpeedsMPS;
         targetingForwardSpeed *= -1.0;
         return targetingForwardSpeed;
